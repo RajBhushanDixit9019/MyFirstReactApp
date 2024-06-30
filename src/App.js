f2aea4f69c78e28b62870ba9800e0bd80d75a610
Raj Bhushan Dixit <rajbhushandixit99@gmail.com>
2024-06-30 23:09:55 +0530

    Created User greeting functionality.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-// getting css styling from the App.css
+import React, { useState } from "react";
 import "./App.css";
 
-
 export default function GreetUser() {
-  function greetuser(username){
-    alert(`Hello ${username}`);
-  }
+  const [name, setName] = useState("");
+
+  const handleInputChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const greetUser = () => {
+    alert(`Hello, ${name}!`);
+    
+  };
+
   return (
-    <div class="form-container">
-      <label for="username" class="form-label">
+    <div className="form-container">
+      <label htmlFor="username" className="form-label">
         Name:
       </label>
-      <input type="text" id="username" class="form-input" placeholder="Your Name..." />
-      <button class="form-button" onclick={greetuser}>Submit</button>
+      <input
+        type="text"
+        id="username"
+        className="form-input"
+        placeholder="Your Name..."
+        value={name}
+        onChange={handleInputChange}
+      />
+      <button className="form-button" onClick={greetUser}>
+        Submit
+      </button>
     </div>
   );
 }
-
